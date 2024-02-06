4fd32a774808dde53e9ece2afa70841c640b446c
lauriehillier <laurie.hillier@gmail.com>
2024-02-06 11:05:15 +0000

    add user context and temp default user

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,12 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { UserProvider } from "./contexts/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </BrowserRouter>
 );
